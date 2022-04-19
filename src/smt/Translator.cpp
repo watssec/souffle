30c5d2a4ff5f774d7048df226c813996ecbb61f6
Meng Xu <meng.xu.cs@uwaterloo.ca>
2022-04-19 17:43:27 -0400

    x

--- src/smt/Translator.cpp
+++ src/smt/Translator.cpp
@@ -8,8 +8,20 @@ namespace souffle::smt {
 
 void Translator::convert(const ast::TranslationUnit& translationUnit) {
     auto& program = translationUnit.getProgram();
+
+    std::cout << "[" << program.getTypes().size() << " types]" << std::endl;
     for (auto type : program.getTypes()) {
         std::cout << *type << std::endl;
+    }
+
+    std::cout << "[" << program.getRelations().size() << " relations]" << std::endl;
+    for (auto rel : program.getRelations()) {
+        std::cout << *rel << std::endl;
+    }
+
+    std::cout << "[" << program.getClauses().size() << " clauses]" << std::endl;
+    for (auto clause : program.getClauses()) {
+        std::cout << *clause << std::endl;
     }
 }
 
