c301ca87ff1c24410fd72bba26c0c32695c5295c
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2022-04-28 15:06:55 +1000

    Fix magic pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -530,13 +530,13 @@ int main(int argc, char** argv) {
                     mk<ast::transform::RemoveRedundantRelationsTransformer>());
 
     // Magic-Set pipeline
-    auto magicPipeline = mk<ast::transform::ConditionalTransformer>(Global::config().has("magic-transform"),
-            mk<ast::transform::PipelineTransformer>(mk<ast::transform::ExpandEqrelsTransformer>(),
-                    mk<ast::transform::MagicSetTransformer>(),
-                    mk<ast::transform::ResolveAliasesTransformer>(),
-                    mk<ast::transform::RemoveRelationCopiesTransformer>(),
-                    mk<ast::transform::RemoveEmptyRelationsTransformer>(),
-                    mk<ast::transform::RemoveRedundantRelationsTransformer>(), clone(equivalencePipeline)));
+    auto magicPipeline = mk<ast::transform::PipelineTransformer>(
+            mk<ast::transform::ConditionalTransformer>(
+                    Global::config().has("magic-transform"), mk<ast::transform::ExpandEqrelsTransformer>()),
+            mk<ast::transform::MagicSetTransformer>(), mk<ast::transform::ResolveAliasesTransformer>(),
+            mk<ast::transform::RemoveRelationCopiesTransformer>(),
+            mk<ast::transform::RemoveEmptyRelationsTransformer>(),
+            mk<ast::transform::RemoveRedundantRelationsTransformer>(), clone(equivalencePipeline));
 
     // Partitioning pipeline
     auto partitionPipeline =
